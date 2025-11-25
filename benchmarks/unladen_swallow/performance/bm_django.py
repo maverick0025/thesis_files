341efc7eec168b9bf4be820d1fc02dd5cbf37e2f
Satya Dowluri <dowlurisatyaashok@gmail.com>
2025-11-25 17:42:07 -0600

    add unladen

---
+++ benchmarks/unladen_swallow/performance/bm_django.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+"""Wrapper script for testing the performance of the Django template system.
+
+This is intended to support Unladen Swallow's perf.py
+
+This will have Django generate a 100x100 table as many times as you
+specify (via the -n flag). The raw times to generate the template will be
+dumped to stdout. This is more convenient for Unladen Swallow's uses: it
+allows us to keep all our stats in perf.py.
+"""
+
+__author__ = "collinwinter@google.com (Collin Winter)"
+
+# Python imports
+import optparse
+import time
+import sys
+if sys.version_info[0] > 2:
+    xrange = range
+
+# Local imports
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django")
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django/django")
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django/django/db")
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django/django/http")
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django/django/template")
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django/django/utils")
+sys.path.append("/home/s265d007/Documents/projs/th/benchmarks-branch-default/unladen_swallow/lib/django/django/")
+
+import util
+
+
+
+# Django imports
+import django, django.conf
+django.conf.settings.configure(TEMPLATES=[{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    }])
+django.setup()
+from django.template import Context, Template
+
+
+DJANGO_TMPL = Template("""<table>
+{% for row in table %}
+<tr>{% for col in row %}<td>{{ col|escape }}</td>{% endfor %}</tr>
+{% endfor %}
+</table>
+""")
+
+def test_django(count):
+    table = [xrange(150) for _ in xrange(150)]
+    context = Context({"table": table})
+
+    # Warm up Django.
+    DJANGO_TMPL.render(context)
+    DJANGO_TMPL.render(context)
+
+    times = []
+    for _ in xrange(count):
+        t0 = time.time()
+        data = DJANGO_TMPL.render(context)
+        t1 = time.time()
+        times.append(t1 - t0)
+    return times
+
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser(
+        usage="%prog [options]",
+        description=("Test the performance of Django templates."))
+    util.add_standard_options_to(parser)
+    options, args = parser.parse_args()
+
+    util.run_benchmark(options, options.num_runs, test_django)
---
+++ benchmarks/unladen_swallow/performance/bm_richards.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python2.4
+
+"""perf.py wrapper for the classic Richards benchmark.
+
+See richards.py for copyright and history information.
+"""
+
+__author__ = "fijall@gmail.com (Maciej Fijalkowski)"
+__contact__ = "collinwinter@google.com (Collin Winter)"
+
+
+# Python imports
+import optparse
+import time
+import sys
+if sys.version_info[0] > 2:
+    xrange = range
+
+# Local imports
+import richards
+import util
+
+try:
+    from hwcounter import Timer as HWTimer, count, count_end
+    HAS_HWCOUNTER = True
+except ImportError:
+    HAS_HWCOUNTER = False
+    print("Warning: hwcounter not installed. CPU cycles will not be measured.")
+
+
+def test_richards(iterations):
+    # Warm-up
+    r = richards.Richards()
+    # r.run(iterations=2)
+
+    times = []
+    for _ in xrange(iterations):
+        t0 = time.time()
+        
+        # Start CPU cycle counting
+        if HAS_HWCOUNTER:
+            cycle_start = count()
+                
+        r.run(iterations=1)
+        
+        # End CPU cycle counting
+        if HAS_HWCOUNTER:
+            cycle_end = count_end()
+            cycles = cycle_end - cycle_start
+        else:
+            cycles = None
+            
+        t1 = time.time()
+        times.append((t1 - t0, cycles))
+    return times
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser(
+        usage="%prog [options]",
+        description="Test the performance of the Richards benchmark")
+    util.add_standard_options_to(parser)
+    options, args = parser.parse_args()
+
+    util.run_benchmark(options, options.num_runs, test_richards)
